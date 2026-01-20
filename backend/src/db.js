2bcc67b468d74a46ff541e50dade6dcfb9967cc6
Reinan Silva <152042326+reinansilvaa@users.noreply.github.com>
2026-01-20 18:23:46 -0300

    att

--- backend/src/db.js
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql2/promise";
-
-export const connection = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "Reinan@021",
-    database: "cadastro"
-});
